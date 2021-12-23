64dca0f95d2fb6bfd4069a14a11f1144933138aa
MohamedAli1997 <54535968+MohamedAli1997@users.noreply.github.com>
2021-12-23 14:52:34 +0100

    Update SimplePython.py

--- SimplePython.py
+++ SimplePython.py
@@ -4,5 +4,9 @@ dict ={'color': 'blue', 'fruit': 'apple', 'pet': 'dog'}
 for key in dict:
      print(key, '->', dict[key])
 #length of dictionary 
-
 print(len(dic))
+
+#Array 
+num_array= [1,2,3,4,5,6,7,8,9]
+#print the Array 
+print(num_array)
