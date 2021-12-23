274b2985b036512366d74d88836089c8ae59fae2
MohamedAli1997 <54535968+MohamedAli1997@users.noreply.github.com>
2021-12-23 14:49:03 +0100

    Update SimplePython.py

--- SimplePython.py
+++ SimplePython.py
@@ -1,1 +1,8 @@
 #Dictionary and Array is introduced in this code 
+dict ={'color': 'blue', 'fruit': 'apple', 'pet': 'dog'}
+#Iterate 
+for key in dict:
+     print(key, '->', dict[key])
+#length of dictionary 
+
+print(len(dic))
